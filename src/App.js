2df6bb6491c716d3a295bf90283399f0aaf726c0
ABojo <abojo98@gmail.com>
2021-04-19 17:04:32 -0400

    Added maps to state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { useState } from 'react';
 import baseStyles from './styles/baseStyles.css';
 import Navbar from './components/Navbar';
-import images from './loadMaps';
+import mapObjects from './loadMaps';
 
 function App() {
-  return <Navbar />;
+  const [maps, setMaps] = useState(mapObjects);
+
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 }
 
 export default App;

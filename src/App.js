f32a7781a829e5b3f463c06467370698d395851a
ABojo <abojo98@gmail.com>
2021-04-19 16:24:10 -0400

    Initial commit / added styling and a navbar component

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import baseStyles from './styles/baseStyles.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import navbarStyles from '../styles/navbarStyles.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h1 className="navbar__title">CoD Memory Game</h1>
+    </div>
+  );
+}
+
+export default Navbar;

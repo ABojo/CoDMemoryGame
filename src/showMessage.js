3c431e6612ff9a04a3ad579d5687c7a26feb3bd2
ABojo <abojo98@gmail.com>
2021-04-20 12:34:10 -0400

    Removed duplicate code in the showMessage function

--- src/showMessage.js
+++ src/showMessage.js
@@ -3,13 +3,9 @@ import delay from './delay';
 const showMessage = async (success, message) => {
   const instructions = document.createElement('div');
   instructions.className = 'instructions';
+  instructions.textContent = message;
+  if (!success) instructions.classList.add('lose');
 
-  if (success) {
-    instructions.textContent = message;
-  } else {
-    instructions.classList.add('lose');
-    instructions.textContent = message;
-  }
   document.body.append(instructions);
   await delay(5000);
   instructions.remove();
